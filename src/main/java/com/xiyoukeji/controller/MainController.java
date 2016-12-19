4b119b435df78cecee8957e77b3161bab2969dc8
Delia <dasiy@dasiydeMacBook-Pro.local>
2016-12-19 18:37:47 +0800

    uygyg

---
+++ src/main/java/com/xiyoukeji/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.xiyoukeji.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by dasiy on 16/12/19.
+ */
+@Controller
+@RequestMapping(method = RequestMethod.POST)
+public class MainController {
+    @RequestMapping(value = "/test",method = RequestMethod.POST)
+    @ResponseBody
+    public Map index(){
+        Map<String,Object> map=new HashMap<>();
+        map.put("x","y");
+        return map;
+    }
+}
